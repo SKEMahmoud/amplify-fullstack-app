f7fd39d1aa60b9f2744f94e26e2b2884e4fa8d58
Mahmoud el Sayed <mahmoud.elsayed@simon-kucher.com>
2024-01-10 10:43:31 +0200

    add amplify auth to project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyfullstackapp9a8ee2b4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
